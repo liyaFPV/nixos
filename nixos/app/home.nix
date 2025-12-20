82daee5ad76b2d71fcaeba78d7822e4f3b585b67
liyaFPV <lbkmzc942@gmail.com>
2025-12-20 11:19:43 +0500

    Удалить файл configuration.nix и переместить параметры автоматической очистки в home.nix

--- nixos/app/home.nix
+++ nixos/app/home.nix
@@ -1,6 +1,11 @@
 {config, pkgs, lib, ...}:
 
 {
+  nix.gc = {
+  automatic = true;                 # включает автоматическую очистку
+  dates = "weekly";                 # как часто (можно daily, weekly, monthly)
+  options = "--delete-older-than 30d";  # удалить поколения старше 30 дней
+  };
  services.xserver={
   enable=true;
    displayManager.gdm={
