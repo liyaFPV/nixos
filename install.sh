ace63a5f8126e75bada81cd520b8db5931e57be9
liyaFPV <lbkmzc942@gmail.com>
2025-12-25 11:23:09 +0500

    Обновить конфигурацию Waybar и скрипт установки: добавить модули для отображения часов и рабочих пространств, изменить стиль фона и цвет текста.

--- install.sh
+++ install.sh
@@ -1,2 +1,22 @@
+read -p "Применить системную конфигурацию? (y/n): " answer
 
-sudo nixos-rebuild switch
+case "$answer" in
+  y|Y)
+    echo "Копирую конфиги пользователя..."
+    cp -r .config/* /home/liyavr/.config/
+
+    echo "Копирую конфиги NixOS..."
+    sudo cp -r nixos/* /etc/nixos/
+
+    echo "Пересборка системы..."
+    sudo nixos-rebuild switch
+    ;;
+  n|N)
+    echo "Копирую только пользовательские конфиги..."
+    cp -r .config/* /home/liyavr/.config/
+    ;;
+  *)
+    echo "Ошибка: введите y или n"
+    exit 1
+    ;;
+esac
