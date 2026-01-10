cf3876f4900a1460aa64543d648eb76aafe71d01
liyaFPV <lbkmzc942@gmail.com>
2026-01-10 23:59:37 +0500

    добавлены flake

--- flake.sh
+++ flake.sh
@@ -1,1 +1,3 @@
-nix flake lock
+nix build .#nixosConfigurations.laptop.config.system.build.toplevel
+nix build .#nixosConfigurations.pc.config.system.build.toplevel
+nix build .#nixosConfigurations.vm.config.system.build.toplevel
