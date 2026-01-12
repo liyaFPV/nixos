e919efc91c04817efd7a76e8f69cee9f172c6e75
liyaFPV <lbkmzc942@gmail.com>
2026-01-12 17:08:46 +0000

    обновлён скрипт устоновки

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+set -e
 
 # ---------------------------
 # ASCII-баннер
@@ -32,7 +33,33 @@ echo "1. Laptop"
 echo "2. PC"
 echo "3. VM"
 echo "4. Build flake"
-read -p "Enter number: " host
+read -rp "Enter number: " host
+
+# ---------------------------
+# Первая установка?
+# ---------------------------
+read -rp "Is this your first installation? (y/n): " first_install
+
+CONFIG_FILE="/etc/nixos/configuration.nix"
+EXPERIMENTAL_LINE='nix.settings.experimental-features = [ "nix-command" "flakes" ];'
+
+if [[ "$first_install" =~ ^[Yy]$ ]]; then
+    echo "First installation detected"
+
+    if ! grep -q 'experimental-features' "$CONFIG_FILE"; then
+        echo "Adding experimental Nix features..."
+
+        sudo sed -i '/system.stateVersion = "25.11";/a\
+\
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];' "$CONFIG_FILE"
+
+        sudo nixos-rebuild switch
+    else
+        echo "Experimental features already enabled"
+    fi
+else
+    echo "Not first installation, continuing without changes"
+fi
 
 # ---------------------------
 # Установка базовых файлов
@@ -43,23 +70,30 @@ sudo cp -r wallpapers /home/liyavr/
 # ---------------------------
 # Действия по выбранному хосту
 # ---------------------------
-if [ "$host" = "1" ]; then
-    sudo cp -r host/Laptop /etc/nixos/
-    sudo nixos-rebuild switch --flake .#laptop --extra-experimental-features "nix-command flakes"
-elif [ "$host" = "2" ]; then
-    sudo cp -r host/PC /etc/nixos/
-    sudo nixos-rebuild switch --flake .#pc --extra-experimental-features "nix-command flakes"
-elif [ "$host" = "3" ]; then
-    sudo cp -r host/VM /etc/nixos/
-    sudo nixos-rebuild switch --flake .#vm --extra-experimental-features "nix-command flakes"
-elif [ "$host" = "4" ]; then
-    ./flake.sh
-else
-    echo "Invalid option!"
-fi
+case "$host" in
+    1)
+        sudo cp -r host/Laptop/* /etc/nixos/
+        sudo nixos-rebuild switch --flake .#laptop
+        ;;
+    2)
+        sudo cp -r host/PC/* /etc/nixos/
+        sudo nixos-rebuild switch --flake .#pc
+        ;;
+    3)
+        sudo cp -r host/VM/* /etc/nixos/
+        sudo nixos-rebuild switch --flake .#vm
+        ;;
+    4)
+        ./flake.sh
+        ;;
+    *)
+        echo "Invalid option!"
+        exit 1
+        ;;
+esac
 
 # ---------------------------
 # Завершение
 # ---------------------------
-echo "Installation completed successfully!"
+echo "Installation completed"
 echo "You are successfully isolated from society"
