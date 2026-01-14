f98cab7e7abec28ff41f7db35c8eead67d68450f
liyaFPV <lbkmzc942@gmail.com>
2026-01-14 17:58:21 +0000

    исправлена проблема с загрушиком

---
+++ hardware-configuration.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems = lib.mkForce { };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
