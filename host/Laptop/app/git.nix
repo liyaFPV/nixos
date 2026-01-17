a7b546ab475011dc81b5be549368d6dd8ebe7d9f
liyaFPV <lbkmzc942@gmail.com>
2026-01-17 16:08:07 +0000

    edit git.nix

--- host/Laptop/app/git.nix
+++ host/Laptop/app/git.nix
@@ -1,23 +1,6 @@
 { config, pkgs, ... }:
-
-let
-  home-manager = fetchTarball {
-    url = "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
-  };
-in
 {
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
-
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-
-  home-manager.users.yourUserName = {
-    programs.git = {
-      enable = true;
-      userName  = "LiyaFPV";
-      userEmail = "lbkmzc942@gmail.com";
-    };
-  };
+  programs.git = {
+    enable = true;
+   };
 }
